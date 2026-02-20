0d6fa79c9581d36a7a6fbdf077a61c49e2fcc963
ParryHotter <Pap.Zsombor@C19-01>
2026-02-20 09:44:28 +0100

    Add project files.

---
+++ MCDONALDS/FileManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCDONALDS
+{
+    internal class FileManager
+    {
+        public static List<Mcdonalds> Readfile(string fileName)
+        {
+            List<Mcdonalds> sorok = new List<Mcdonalds>();
+            try
+            {
+                foreach (string sor in File.ReadAllLines(fileName, Encoding.UTF8).Skip(1))
+                {
+                    string[] temp = sor.Split(";");
+
+                    sorok.Add(new Mcdonalds(temp));
+
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            return sorok;
+        }
+    }
+}
---
+++ MCDONALDS/mcdonalds.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCDONALDS
+{
+    public class burger
+    {
+        public string name {  get; set; }
+        public int price {  get; set; }
+        public double kaloria {  get; set; }
+        public bool glutenmentes {  get; set; }
+
+        public burger(string[] data)
+        {
+            this.name = data[0];
+            this.price = int.Parse(data[1]);
+            this.kaloria = double.Parse(data[2]);
+            if (data[3] == "true") { this.glutenmentes = true; }
+            else if (data[6] == "false") { this.glutenmentes = false; }
+            else { this.glutenmentes = false; }
+        }
+    }
+    public class falatkak
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public double kaloria { get; set; }
+        public bool glutenmentes { get; set; }
+
+        public falatkak(string[] data)
+        {
+            this.name = data[0];
+            this.price = int.Parse(data[1]);
+            this.kaloria = double.Parse(data[2]);
+            if (data[3] == "true") { this.glutenmentes = true; }
+            else if (data[6] == "false") { this.glutenmentes = false; }
+            else { this.glutenmentes = false; }
+        }
+    }
+}
